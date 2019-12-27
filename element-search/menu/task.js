5d633607291059c0ba1c35b034240ecbef22696e
VitaliiBo <boflying1@gmail.com>
2019-12-27 17:52:34 +0300

    1.2 2/3 50%

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,29 @@
 const menuCollection = document.querySelectorAll('.menu__item');
 
+const menuLinks = document.querySelectorAll('.menu__item a');
+console.log(menuLinks);
+const menuLinksArr = Array.from(menuLinks);
+console.log(menuLinksArr);
+const menuLinksArrSub = menuLinksArr.filter(item => item.nextElementSibling);
+console.log(menuLinksArrSub);
+menuLinksArrSub.forEach(item => item.href ='#');
 
 
 function subActive() {
   let nextElement = this.lastElementChild;
   let classesList = nextElement.classList;
-  classesList.add('menu_active');
+  if ( nextElement == 'ul.menu.menu_sub' ) {
+    console.log('zaebis');
+  }
+  if (classesList.contains('menu_active')) {
+    classesList.remove('menu_active');
+  } else {
+    classesList.add('menu_active');
+  }
 
 }
 
-menuCollection[1].addEventListener('click', subActive);
+// menuCollection[1].addEventListener('click', subActive);
+menuCollection.forEach( item => item.addEventListener('click', subActive));
+//console.log(menuCollection[1].nextElementSibling.querySelectorAll('.menu_sub'));
 console.log(menuCollection);
