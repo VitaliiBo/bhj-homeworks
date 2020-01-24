63141665f8e9e5503e5ea7f6431fc96175a53b31
VitaliiBo <boflying1@gmail.com>
2020-01-24 08:57:04 +0300

    3.1 2/3 100%

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,3 +1,9 @@
+const div = document.querySelector('#items');
+const loader = document.querySelector('#loader');
+if (localStorage.getItem('valutes') != null) {
+  div.innerHTML = localStorage.getItem('valutes');
+  loader.classList.remove('loader_active');
+}
 function getExchangeRates() {
   let xhr = new XMLHttpRequest();
   let url = 'https://netology-slow-rest.herokuapp.com/'
@@ -6,13 +12,27 @@ function getExchangeRates() {
   xhr.send();
   xhr.onreadystatechange = function () {
     if( xhr.readyState === 4 ){
+      div.innerHTML = '';
       let valutes = xhr.response.response.Valute;
-      for (var key in valutes) {
-        
+      let divTemp;
+      for ( key in valutes) {
+        divTemp =
+        `<div class="item">
+          <div class="item__code">
+              ${valutes[key].CharCode}
+          </div>
+          <div class="item__value">
+              ${valutes[key].Value}
+          </div>
+          <div class="item__currency">
+              руб.
+          </div>
+        </div>`;
+        div.insertAdjacentHTML('beforeEnd', divTemp);
       }
-      console.log( xhr.response.response.Valute );
+      loader.classList.remove('loader_active');
+      localStorage.setItem('valutes' , div.innerHTML);
     }
   }
 }
-
 getExchangeRates();
